e46b5a2c74ed2b439431552ff1ebcdc6f07a0e2d
biplavbarua <innocentsavage.bb@gmail.com>
2024-02-04 21:20:41 +0530

    Stacked Menu Component Completed

--- src/App.js
+++ src/App.js
@@ -3,25 +3,30 @@ import Accordian from "./components/accordian";
 import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load-more-data";
 import RandomColor from "./components/random-color";
+import StackedMenu from "./components/stacked-menu";
+import menus from "./components/stacked-menu/data";
 import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
       {/* Accordian Component */}
-      {/* <Accordian /> */}
+      <Accordian />
 
       {/* Random Color Component */}
-      {/* <RandomColor /> */}
+      <RandomColor />
 
       {/* {Star - Rating Component} */}
-      {/* <StarRating noOfStars={5}/> */}
+      <StarRating noOfStars={5}/>
 
       {/* Image Slider Component */}
-      {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={"10"} /> */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={"10"} />
 
       {/* Load More Products Component */}
       <LoadMoreData />
+
+      {/* Stacked Menu Component */}
+      <StackedMenu menus={menus}/>
     </div>
   );
 }
